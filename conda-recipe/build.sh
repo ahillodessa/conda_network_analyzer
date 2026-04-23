72d6daba74976365a52773f1d4b4b2b9d63d3f28
ahillodessa <ahillodessa@gmail.com>
2026-04-23 13:27:18 +0300

    Add initial conda configuration
    - add local configuration
    - add global configuration
    - add build.sh and bld.bat

---
+++ conda-recipe/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p build && cd $_
+
+cmake .. \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    ${CMAKE_ARGS}
+
+ninja
+ninja install
